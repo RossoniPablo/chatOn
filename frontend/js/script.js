a720506f7fa6d1bee15345ad4c54c4ff853918b5
Pablo Rossoni <pablorossoni107@outlook.com>
2024-05-31 20:46:47 -0300

    test

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,15 +1,13 @@
+// login elements
+const login = document.querySelector(".login")
+const loginForm = login.querySelector(".login-form")
+const loginInput = login.querySelector(".login-input")
 
-//Elementos do login
-const login = document.querySelector('.login')
-const loginForm = login.querySelector('.login-form')
-const loginInput = login.querySelector('.login-input')
-
-//Elementos do chat
-const chat = document.querySelector('.chat')
-const chatForm = chat.querySelector('.chat-form')
-const chatInput = chat.querySelector('.chat-input')
-const chatMessages = chat.querySelector('.chat-messages')
-
+// chat elements
+const chat = document.querySelector(".chat")
+const chatForm = chat.querySelector(".chat-form")
+const chatInput = chat.querySelector(".chat-input")
+const chatMessages = chat.querySelector(".chat-messages")
 
 const colors = [
   "cadetblue",
@@ -20,56 +18,49 @@ const colors = [
   "gold"
 ]
 
-const getRandomColor = () => {
-  const randomIndex = Math.floor(Math.random() * colors.length)
-  return colors[randomIndex]
-}
-//Atualizando os valores do usuário
-const user = {
-  id: "",
-  name: "",
-  color: ""
-}
+const user = { id: "", name: "", color: "" }
 
 let websocket
 
-//Cria o 'balão' da mensagem
 const createMessageSelfElement = (content) => {
-  const div = document.createElement('div')
-  div.classList.add('message-self')
+  const div = document.createElement("div")
 
+  div.classList.add("message-self")
   div.innerHTML = content
+
   return div
 }
 
 const createMessageOtherElement = (content, sender, senderColor) => {
-  const div = document.createElement('div')
-  const span = document.createElement('span')
+  const div = document.createElement("div")
+  const span = document.createElement("span")
 
+  div.classList.add("message-other")
 
-
-
-  div.classList.add('message-other')
-  span.classList.add('message-sender')
+  span.classList.add("message-sender")
   span.style.color = senderColor
 
   div.appendChild(span)
-  span.innerHTML = sender
 
+  span.innerHTML = sender
   div.innerHTML += content
+
   return div
+}
+
+const getRandomColor = () => {
+  const randomIndex = Math.floor(Math.random() * colors.length)
+  return colors[randomIndex]
 }
 
 const scrollScreen = () => {
   window.scrollTo({
     top: document.body.scrollHeight,
-    behavior: 'smooth'
+    behavior: "smooth"
   })
 }
 
-//Recebe a mensagem do servidor  e mostra na tela
 const processMessage = ({ data }) => {
-  //Dados que vem do servidor
   const { userId, userName, userColor, content } = JSON.parse(data)
 
   const message =
@@ -77,30 +68,25 @@ const processMessage = ({ data }) => {
       ? createMessageSelfElement(content)
       : createMessageOtherElement(content, userName, userColor)
 
-
-  const element = createMessageOtherElement(content, userName, userColor)
-
-  //Coloca a mensagem na tela
   chatMessages.appendChild(message)
-  scrollScreen()
 
+  scrollScreen()
 }
 
 const handleLogin = (event) => {
   event.preventDefault()
-  user.id = Date.now()
+
+  user.id = crypto.randomUUID()
   user.name = loginInput.value
   user.color = getRandomColor()
 
-  login.style.display = 'none'
-  chat.style.display = 'flex'
+  login.style.display = "none"
+  chat.style.display = "flex"
 
-  //Criando conexão, utilizando render para subir online
-  websocket = new WebSocket('ws://chaton-backend-7x1m.onrender.com')
+  websocket = new WebSocket("wss://chaton-backend-7x1m.onrender.com")
   websocket.onmessage = processMessage
 }
 
-//Envia mensagem para o servidor
 const sendMessage = (event) => {
   event.preventDefault()
 
@@ -112,8 +98,9 @@ const sendMessage = (event) => {
   }
 
   websocket.send(JSON.stringify(message))
+
   chatInput.value = ""
 }
 
-loginForm.addEventListener('submit', handleLogin)
-chatForm.addEventListener('submit', sendMessage)
+loginForm.addEventListener("submit", handleLogin)
+chatForm.addEventListener("submit", sendMessage)
